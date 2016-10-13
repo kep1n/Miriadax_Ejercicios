8a91c11d5f650a8cf510e3750ba5dc39e0d0501c
kep1n <kepaalp@gmail.com>
2016-10-13 18:40:52 +0200

    Incorporación de últimas funciones para dar función a la calculadora.

--- modulo4/js/calc.js
+++ modulo4/js/calc.js
@@ -1,34 +1,39 @@
 
+	var num, accum = 0, op = "";
+
 	function vaciar() {
 		document.getElementById("input").value = "";
 	}
 
 	function pow() {
-		var num = document.getElementById("input");
-		var inputNumber = num.value;
+		num = document.getElementById("input");
+		var inputNumber = num.value; //Para mantener el número visible en el input
 		num.value = Math.pow(num.value,2);
 		document.getElementById("result").value = num.value;
 		document.getElementById("input").value = inputNumber;
+		
 	}
 
 	function inverse() {
-		var num = document.getElementById("input");
-		var inputNumber = num.value;
+		num = document.getElementById("input");
+		var inputNumber = num.value; //Para mantener el número visible en el input
 		num.value = 1/num.value;
 		document.getElementById("result").value = num.value;
 		document.getElementById("input").value = inputNumber;
+		
 	}
 
 	function sqrt() {
-		var num = document.getElementById("input");
-		var inputNumber = num.value;
+		num = document.getElementById("input");
+		var inputNumber = num.value; 
 		num.value = Math.sqrt(num.value);
 		document.getElementById("result").value = num.value;
 		document.getElementById("input").value = inputNumber;
+		
 	}
 
 	function int() {
-		var num = document.getElementById("input");
+		num = document.getElementById("input");
 		var inputNumber = num.value;
 		if (num.value >= 0) {
 			num.value = Math.floor(num.value);
@@ -38,4 +43,55 @@
 			document.getElementById("result").value = num.value;
 		}
 		document.getElementById("input").value = inputNumber;
+		
+	}
+
+	function sum() {
+		op = "+";
+		num = document.getElementById("input");
+		accum = num.value;
+
+	}
+
+	function subs() {
+		op = "-";
+		num = document.getElementById("input");
+		accum = num.value;
+
+	}
+
+	function powy() {
+		op = "**";
+		num = document.getElementById("input");
+		accum = num.value;
+
+	}
+
+	function calc() {
+		if (op === '+'){
+			num.value =  (+accum + +num.value)
+			document.getElementById("result").value = num.value;
+			accum = num.value;
+			vaciar();
+		}
+		else if (op ==='-') {
+			num.value = (+accum - +num.value)
+			document.getElementById("result").value = num.value;
+			accum = num.value;
+			vaciar();
+		}
+		else if (op === '**') {
+			num.value = (Math.pow(+accum, +num.value))
+			document.getElementById("result").value = num.value;
+			accum = num.value;
+			vaciar();
+		}
+	}
+
+	function limpiar () {
+		document.getElementById("result").value = "";
+		document.getElementById("input").value="";
+		accum = 0;
+		op = "";
+		num  = 0;
 	}
